c19cfdc77466642771ff4e1ada7e55e262088a4e
Baige <82246167+HAS-1314@users.noreply.github.com>
2025-04-04 17:53:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'dist',
   site: "https://www.aibar.app",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   adapter: vercel(),
